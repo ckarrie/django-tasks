75aa6e8a6f44ca2f9c982b57da3b34a1961230f7
Christian Karri? <ckarrie@gmail.com>
2013-06-30 18:29:02 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-tasks',
+      version='1.0',
+      description='Django Tasks',
+      author='farialima@gmail.com ',
+      author_email='farialima@gmail.com',
+      url='http://code.google.com/p/django-tasks/',
+      packages=['djangotasks',],
+     )
